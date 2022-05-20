97ea1500b5498735707ccd062e8c9ee2ad5a5c31
blackwoods358 <65369957+blackwoods358@users.noreply.github.com>
2022-05-20 09:22:17 -0500

    Add files via upload

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -1,7 +1,15 @@
 package com.company;
+/**
 
+ * Fibonacci class used for findinng the corresponding fibonacci num
+ * @author Created by stephen Blackwood on 05/20/2022
+ */
 public class Fibonacci {
-
+    /**
+     *
+     * @param  n  is going to be any int the user inputs
+     *  @return going to return a long because the numbers tend to grow at a very fast rate
+     */
      static long fibonacci(int n) {
       //base case (to stop infinite calls)
         if(n==0)
